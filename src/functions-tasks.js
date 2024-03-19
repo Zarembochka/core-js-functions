c3f2499e8c44657a5b6041d88e314331366b9955
Luba Hadunova <luba.godunova2018@gmail.com>
2024-03-19 20:28:11 +0100

    feat: add solution for logger task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -187,8 +187,16 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function fn(...args) {
+    const parametres = args.map((item) =>
+      Array.isArray(item) ? JSON.stringify(item) : item
+    );
+    logFunc(`${func.name}(${parametres}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${parametres}) ends`);
+    return result;
+  };
 }
 
 /**
