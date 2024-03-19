c52f6e6652c9368e648c9bcb6e5b2867557ad7bd
Luba Hadunova <luba.godunova2018@gmail.com>
2024-03-19 21:40:46 +0100

    feat: add solution for getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -235,8 +235,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let start = startFrom;
+  return function fn() {
+    const result = start;
+    start += 1;
+    return result;
+  };
 }
 
 module.exports = {
